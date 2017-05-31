2967585ded622561cdb78b7cb90d1cbd1ecef683
Mads-Pc\Mads <madsmarzec@hotmail.com>
2017-05-31 22:03:34 +0200

    Oprettet fragments på Forsiden og Fixtures

--- VemmelevBoldklub/app/src/main/java/com/example/madsm/vemmelevboldklub/MainActivity.java
+++ VemmelevBoldklub/app/src/main/java/com/example/madsm/vemmelevboldklub/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.madsm.vemmelevboldklub;
 
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v4.view.GravityCompat;
@@ -15,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
     private Toolbar mToolbar;
+    FragmentTransaction fragmentTransaction;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,38 @@ public class MainActivity extends AppCompatActivity {
         mToggle.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.main_container, new HomeFragment());
+        fragmentTransaction.commit();
+        getSupportActionBar().setTitle("Home Fragment...");
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_frontPage:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container, new HomeFragment());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Home Fragment");
+                        item.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        break;
+
+                    case R.id.nav_fixtures:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container, new FixtureFragment());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Fixture Fragment");
+                        item.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        break;
+                }
+
+                return false;
+            }
+        });
     }
 
     @Override
